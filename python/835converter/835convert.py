bdf532877f0b5508344b554a318cb81e1c3b005e
Thorsten Vogt <tvogt@mimirhealth.com>
2015-07-17 17:32:46 -0500

    Python 835converter
    Wow
    much debugging
    such better

--- python/835converter/835convert.py
+++ python/835converter/835convert.py
@@ -36,28 +36,52 @@ def returnsegm(text,segmentcode):
 def returnelem(text,n):
     ## returns the nth element in input text
     elementcnt=text.count('*')
-    if n<elementcnt:
+    if text=='' or text==None:
+        return ''
+    elif n<elementcnt:
         elementstart=text.replace('*', 'X', n-1).find('*')+1
         elementend=text.replace('*','X',n).find("*")
         return text[elementstart:elementend]
     elif n==elementcnt:
         elementstart=text.rfind("*")+1
         return text[elementstart:]
-    else:
-        return "n/a"
+
 
 def returnelement(text,segcode,elemno):
     ## returns element number elemno out of the segment named segcode in the input text
     return returnelem(returnsegm(text,segcode),elemno)
 
+def returnsubelement(text,number):
+   ## returns a subelement (colon separated) out of text
+   subcnt=text.count(':')+1
+   if subcnt==1 and number>1:
+      return ''
+   elif number<subcnt:
+      if number==1:
+         substart=0
+         subend=text.find(':')
+      elif number > 1:
+         substart=text.replace(':', 'X', number-2).find(':')+1
+         subend=text.replace(':','X',number-1).find(':')
+      return text[substart:subend]
+   elif number==subcnt:
+      substart=text.rfind(':')+1
+      return text[substart:]
+   else:
+      return ''
+
+
    
 def convdate(a):
     ## converts yyyymmdd to yyyy-mm-dd
-   return a[0:4]+"-"+a[4:6]+"-"+a[6:8]
+   if a==None:
+      return ""
+   else:
+      return a[0:4]+"-"+a[4:6]+"-"+a[6:8]
 
 ### VARIABLES
 
-path = "D:/"  # where the .x12 files are stored.
+path = "C:/TestData/"  # where the .x12 files are stored.
 n=0  # Counter
 
 data=[]
@@ -67,30 +91,29 @@ datastructure = [
 "Transact No","Filename","Prod Date"		        		## 0-2		TRN*: 2   FILENAME, DTM*405
 ,"Pat Ctrl No","Payer Clm No","Line Item No"		        	## 3-5		CLP*: 1,7   REF*6R*:  3
 ,"Payer Nm", "Payer Id", "Payer City", "Payer State","Crossover Payer"	## 6-10		N1*PR*:  2,4  N4*: 1,2  NM1*TT:  3
-,"Product Type",''								## 11-12	REF*CE*:   2
+,"Product Type",''							## 11-12	REF*CE*:   2
 ,"Payee Nm", "Payee id", "Payee City", "Payee State"			## 13-16	N1*PE*:  2,4  N4*: 1,2
 ,"Pat Last Nm","Pat First Nm","Pat Mid Init","Pat Id"            	## 17-20	NM1*QC*: 3,4,5,9
 ,"Prov Last Nm","Prov First Nm","Prov Mid Init", "Prov Id"        	## 21-24	NM1*82*: 3,4,5,9
-,"Srv Date","Srv Code"  						## 25-26	DTM*472: 2  SVC* 1 seg 2
-,"Amt chrgd", "Clm Date"						## 27-28	SVC* 2      DTM*050* 2
-,"Amt paid", "Pmt Date"	        					## 29-30	SCV* 3      BPR*:    16
-,"CA Grp 1","CA Rsn 1","CA Amt 1"				        ## 31-33	CAS* 1,2,3
-,"CA Grp 2","CA Rsn 2","CA Amt 2"				        ## 34-36	
-,"CA Grp 3","CA Rsn 3","CA Amt 3"				        ## 37-39	
-,"CA Grp 4","CA Rsn 4","CA Amt 4"				        ## 40-42	
-,"CA Grp 5","CA Rsn 5","CA Amt 5"				        ## 43-45	
-,"CA Grp 6","CA Rsn 6","CA Amt 6"				        ## 46-48
-,"Clm Chrg","Clm Pmt", "Clm Pat"]					## 49-51	CLP 3,4,5
-
+,"Srv Date","Srv Code","Srv Cd Mod1","Srv Cd Mod2"			## 25-28	DTM*472: 2  SVC* 1 seg 2
+,"Amt chrgd", "Clm Date"						## 29-30	SVC* 2      DTM*050* 2
+,"Amt paid", "Pmt Date"	        					## 31-32	SCV* 3      BPR*:    16
+,"CA Grp 1","CA Rsn 1","CA Amt 1"				        ## 33-35	CAS* 1,2,3
+,"CA Grp 2","CA Rsn 2","CA Amt 2"				        ## 36-38	
+,"CA Grp 3","CA Rsn 3","CA Amt 3"				        ## 39-41	
+,"CA Grp 4","CA Rsn 4","CA Amt 4"				        ## 42-44	
+,"CA Grp 5","CA Rsn 5","CA Amt 5"				        ## 45-47	
+,"CA Grp 6","CA Rsn 6","CA Amt 6"				        ## 48-50
+,"Clm Chrg","Clm Pmt", "Clm Pat"]					## 51-53	CLP 3,4,5
 
 
    
-## add datastructure to data
+      ## add datarow to data
 data.append(datastructure)
 
 
 ## this is the intermediate storage where data is collected for each line item
-datarow=['']*52
+datarow=['']*54
 
 
 
@@ -140,11 +163,11 @@ for infile in listing:
            datarow[9]=returnelement(transact,"N4*",2)
            
            ## Extract Payee Information
-           datarow[13]=returnelement(transact,"N1*PR*",2)
-           datarow[14]=returnelement(transact,"N1*PR*",4)
+           datarow[13]=returnelement(transact,"N1*PE*",2)
+           datarow[14]=returnelement(transact,"N1*PE*",4)
 
            ## Extract Payment Date 
-           datarow[30]=convdate(returnelement(transact,"BPR*",16))
+           datarow[32]=convdate(returnelement(transact,"BPR*",16))
            
            ## Find number of claims in transaction set
            cntclaim=transact.count("~CLP")
@@ -184,11 +207,13 @@ for infile in listing:
             datarow[11]=returnelement(claim,"REF*CE*",2)
 
             ## Extract Claim Payment Info
-            datarow[49]=returnelement(claim,"CLP*",3)
-            datarow[50]=returnelement(claim,"CLP*",4)
-            datarow[51]=returnelement(claim,"CLP*",5)
+            datarow[51]=returnelement(claim,"CLP",3)
+            datarow[52]=returnelement(claim,"CLP",4)
+            datarow[53]=returnelement(claim,"CLP",5)
 
-
+            
+            ## Extract Claim Date
+            datarow[30]=convdate(returnelement(claim,"DTM*050*",2))
           
             ## Find number of service line items in claim
             cntitem=claim.count("~SVC")
@@ -207,58 +232,52 @@ for infile in listing:
 
 
                 ## Extract Service Line Item Number
-                datarow[5]=returnelement(item,"REF*6R*",2)
+                datarow[5]=str(returnelement(item,"REF*6R*",2))
 
                 ## Extract Service Date
                 datarow[25]=convdate(returnelement(item,"DTM*472*",2))
-                ## Extract Service Code
-                datarow[26]=returnelement(item,"SVC*",1)[3:]
-
-                ## Extract Claim Date
-                datarow[28]=convdate(returnelement(item,"DTM*050*",2))
+                ## Extract Service Code & Code Modifiers
+                datarow[26]=returnsubelement((returnelement(item,"SVC*",1)[3:]),1)
+                datarow[27]=returnsubelement((returnelement(item,"SVC*",1)[3:]),2)
+                datarow[28]=returnsubelement((returnelement(item,"SVC*",1)[3:]),3)
 
                 ## Extract Amount Charged
-                datarow[27]=returnelement(item,"SVC*",2)
+                datarow[29]=returnelement(item,"SVC*",2)
 
                 ## Extract Amount Paid
-                datarow[29]=returnelement(item,"SVC*",3)
+                datarow[31]=returnelement(item,"SVC*",3)
 
                 ## Find number of claim adjustments for each item (max 6)
                 cntadj=item.count("~CAS")
 
-                # define a list each for ca group code, ca reason code & ca amount 
-                ca_group_code=[0]*6
-                ca_reason_code=[0]*6
-                ca_amount=[0]*6
-
                 
                 ## Iterate over all claim adjustment CAS for each item
-                for l in range(0,cntadj):
-                    adjinitem=l
-                    if l<cntadj:
+                for l in range(0,6):
+                    
+                    if l<cntadj-1:
                         adjstart=item.replace("~CAS", "XXXX", l).find("~CAS")+1
                         adjend=item.replace("~CAS", "XXXX", l+1).find("~CAS")
                         adj=item[adjstart:adjend]
-                    elif l==cntadj:
-                        adjstart=item.rfind("~CAS")+1
-                        adj=item[itemstart:]
+                    elif l==cntadj-1:
+                        adjstart=item.replace("~CAS", "XXXX", l).find("~CAS")+1
+                        adjhelp=item[adjstart:]
+                        adj=adjhelp[:(adjhelp.find("~"))]    ## <-- careful, following element varies, so only the ~-sign as terminator!
+                    elif l>=cntadj:
+                        adj=''
+                        
 
                     ## Extract data for claim adjustment
                         
                     ## Extract CA Group Code
-                    datarow[31+l*3]=returnelem(adj,2)
+                    datarow[33+l*3]=returnelem(adj,1)
                     ## Extract CA Reason Code
-                    datarow[32+l*3]=returnelem(adj,3)
+                    datarow[34+l*3]=returnelem(adj,2)
                     ## Extract CA Amount
-                    datarow[33+l*3]=returnelem(adj,4)   
-
-                ## Make a copy of datarow to write into data
-                    # This is important; otherwise all the datarows will be identical; when a pointer changes,
-                    # ALL the rows already in data change as well
-                update=datarow[:]
+                    datarow[35+l*3]=returnelem(adj,3)   
 
                     
-                ## add update to data
+                ## add string to data
+                update=datarow[:]
                 data.append(update)
                 
                 
@@ -271,7 +290,7 @@ a = csv.writer(b)
 a.writerows(data)
 b.close()
 
-
+print "done."
 
 
 
